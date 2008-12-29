04d3cb8eaeac105969b7bf536ff085e73dbea2a7
Ben Askins <ben.askins@gmail.com>
2008-12-29 17:46:07 +1100

    Move 'value->pixel' calculations into yToPx() and xToPx() functions. Hopefully this will simplify catering for negative values. Also, gave the yAxis caption a little breathing space.

--- js/simplegraph.js
+++ js/simplegraph.js
@@ -52,7 +52,7 @@ function SimpleGraph(target, labels, data) {
     fillColor: "#000",
     fillOpacity: 0.2,
     // -- Hover
-    addHover: true,
+    addHover: true
   }, (arguments[3] || {}) );
 
 
@@ -125,9 +125,10 @@ function SimpleGraph(target, labels, data) {
     ).attr({stroke: this.settings.lineColor, fill: this.settings.lineColor, opacity: 0.3}); //TODO PARAMS - legend border and fill style
 
     for (var i = 1, ii = (this.rows) + 1; i < ii; i = i + 2) {
-      var y = this.topGridEdge + ((i - 1) * this.gridHeight / this.rows + 2),
-          x = this.leftGridEdge - (10 + this.settings.yAxisOffset),
-          t = r.text(x, y, (ii - i)*2).attr(this.yAxisLabelStyle);        
+      var value = (ii - i)*2,
+          y     = this.yToPx(value) + 4, // TODO: Value of 4 works for default dimensions, expect will need to scale
+          x     = this.leftGridEdge - (6 + this.settings.yAxisOffset),
+          t     = r.text(x, y, value).attr(this.yAxisLabelStyle);        
     }
     var caption = r.text(
       this.leftGridEdge - (20 + this.settings.yAxisOffset), 
@@ -141,9 +142,17 @@ function SimpleGraph(target, labels, data) {
   
   this.labelXAxis = function() {
     for (var i = 0, ii = this.labels.length; i < ii; i++) {
-        var x = this.settings.leftGutter + this.X * i,
+        var x = this.xToPx(i),
             t = r.text(x, this.settings.height - 6, this.labels[i]).attr(this.xAxisLabelStyle).toBack();
     }
+  };
+  
+  this.yToPx = function(y) {
+    return this.settings.height - this.settings.bottomGutter - this.Y * y;
+  };
+  
+  this.xToPx = function(x) {
+    return this.settings.leftGutter + this.X * x;
   };
   
   // Plot points on the graph
@@ -170,8 +179,8 @@ function SimpleGraph(target, labels, data) {
 
     // Plot the points
     for (var i = 0, ii = this.labels.length; i < ii; i++) {
-        var y = this.settings.height - this.settings.bottomGutter - this.Y * this.data[i],
-            x = this.settings.leftGutter + this.X * i;
+        var y = this.yToPx(this.data[i]),
+            x = this.xToPx(i);
 
         if (this.settings.drawPoints) {
           var dot  = dots.circle(x, y, this.settings.pointRadius).attr({fill: this.settings.pointColor, stroke: "#fff"});
